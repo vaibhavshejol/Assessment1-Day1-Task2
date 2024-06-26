68eb1300ad4d139e51f4701eb1d8de86b435e298
Vaibhav Shejol <vaibhav.shejol@bnt.local>
2024-06-26 13:01:56 +0530

    created project structure or template for task 2 on calculations

---
+++ Task2Calculations/src/main/java/assessment1/Task2CalculationsApplication.java
@@ -0,0 +1,14 @@
+package assessment1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Task2CalculationsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Task2CalculationsApplication.class, args);
+		System.out.println("Application started successfully.....");
+	}
+
+}
---
+++ Task2Calculations/src/test/java/assessment1/Task2CalculationsApplicationTests.java
@@ -0,0 +1,13 @@
+package assessment1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Task2CalculationsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
